98f085efe984bbb70a32b028061bce151786d476
cemreis0 <cemmreis@gmail.com>
2022-04-22 14:28:28 +0300

    added reload button

--- hg-erp.py
+++ hg-erp.py
@@ -11,6 +11,8 @@ root.geometry("1100x500")
 app_name = Label(root, text="app_name")
 app_name.grid(row=0, column=0)
 
+entry = Entry(root, width=25)
+
 def readdb(db_name, index, line):
     db = open(db_name)
     db_line = db.readlines()
@@ -21,18 +23,27 @@ def readdb(db_name, index, line):
     db.close()
     return db_spec_item
 
-def open_url():
-    webbrowser.open_new(readdb(db_name,j,i))
+def update():
+    for widget in root.winfo_children():
+        widget.destroy()
+    main()
 
-for i in range(0,column):
-    for j in range(0,7):
-        if j == 6:
-            url_button = Button(root, text=readdb(db_name, j, i), command=open_url)
-            url_button.grid(row=i+1,column=j,padx=10,pady=10)
-        else:
-            entry = Entry(root, width=25)
-            entry.insert("0", readdb(db_name,j,i))
-            entry.config(state=DISABLED)
-            entry.grid(row=i+1, column=j, padx=10, pady=10)
 
+def main():
+    def open_url():
+        webbrowser.open_new(readdb(db_name, j, i))
+    for i in range(0,column):
+        for j in range(0,7):
+            if j == 6:
+                url_button = Button(root, text=readdb(db_name, j, i), command=open_url, fg="blue")
+                url_button.grid(row=i+1,column=j,padx=10,pady=10)
+            else:
+                entry = Entry(root, width=25)
+                entry.insert("0", readdb(db_name,j,i))
+                entry.config(state=DISABLED)
+                entry.grid(row=i+1, column=j, padx=10, pady=10)
+    reload_button = Button(root, text="RELOAD", command=update, fg="white", bg="blue", height=2, width=10)
+    reload_button.grid(row=i+2, column=3, padx=10, pady=10)
+
+main()
 tkinter.mainloop()
